8a7c48ab20c6ae5a077dae4bd05270e7d55870e9
telito <adrieltelito@Outlook.com>
2020-03-15 20:05:44 -0300

    Agora o sistema possui opções de deletar e editar cidades.

--- script.js
+++ script.js
@@ -23,83 +23,97 @@ function iniciamodal(cidade, estado, ibge, populacao, obser = "") {
     document.querySelector("#cidade").innerHTML = `${cidade}, ${estado}`;
     document.querySelector('#ibge').innerHTML = `Codigo do IBGE: ${ibge}`;
     document.querySelector('#populacao').innerHTML = `População estimada: ${populacao}`
+    document.querySelector('#controle').innerHTML = cidade
 
 
-        replac = cidade.replace(" ", "%20")
-        var req = new XMLHttpRequest();
-      
-        req.onload = mostraApi;
-        req.open("get", `http://api.openweathermap.org/data/2.5/forecast?q=${replac},rs,br&units=metric&lang=pt_br&APPID=c01a54301797a4147060a02f43da82c7`, true);
-        req.send();
-    
-        function mostraApi(){
-            
-            requi = this.responseText
-            requi = JSON.parse(requi)
-            console.log(requi)
-            console.log(requi.list[5].weather[0]);
-            console.log(requi.list[5].weather[0]['description']);
 
-            //Enviando dados para o modal
 
-            
-            for(i = 0; i <= 29; i +=7){
+    replac = cidade.replace(" ", "%20")
+    var req = new XMLHttpRequest();
+
+    req.onload = mostraApi;
+    req.open("get", `http://api.openweathermap.org/data/2.5/forecast?q=${replac},rs,br&units=metric&lang=pt_br&APPID=c01a54301797a4147060a02f43da82c7`, true);
+    req.send();
+
+    function mostraApi() {
+
+        requi = this.responseText
+        requi = JSON.parse(requi)
+
+
+        //Enviando dados para o modal
+
+
+        for (i = 0; i <= 29; i += 7) {
             tempo = requi.list[i].weather[0]['description']
             tempmin = requi.list[i].main.temp_min
             tempmax = requi.list[i].main.temp_max
-            dia  = requi.list[i].dt_txt[8] + requi.list[i].dt_txt[9] + "/" + requi.list[i].dt_txt[5] + requi.list[i].dt_txt[6]
-               
-           
-            
-                if(i == 0){
-                    i = 1
-                    document.querySelector('#dia').innerHTML += `<div class="col col-5" onclick="alteramodal('${tempo}','${tempmin}','${tempmax}','${dia}')">
+            dia = requi.list[i].dt_txt[8] + requi.list[i].dt_txt[9] + "/" + requi.list[i].dt_txt[5] + requi.list[i].dt_txt[6]
+
+            //enviando imagem inicial do modal
+
+            if (tempo == "nublado") {
+                document.querySelector('#modalimg').src = "./img/nublado.jpg"
+            }
+            else if (tempo == "céu limpo") {
+                document.querySelector('#modalimg').src = "./img/limpo.jpg"
+            }
+            else if (tempo == "chuva leve") {
+                document.querySelector('#modalimg').src = "./img/chuva.jpg"
+            }
+            else {
+                document.querySelector('#modalimg').src = "./img/dispersas.jpg"
+            }
+
+            if (i == 0) {
+                i = 1
+                document.querySelector('#dia').innerHTML += `<div class="col col-5" onclick="alteramodal('${tempo}','${tempmin}','${tempmax}','${dia}')">
                                                             <div class="card">
                                                                 <div id="Dia">
                                                                     ${dia}
                                                                 </div>
                                                             </div>
                                                         </div>`
-                   
-                                                        
 
-                }
-                else{
 
-                    document.querySelector('#dia').innerHTML += `<div class="col col-5 col-e" onclick="alteramodal('${tempo}','${tempmin}','${tempmax}','${dia}')">
+
+            }
+            else {
+
+                document.querySelector('#dia').innerHTML += `<div class="col col-5 col-e" onclick="alteramodal('${tempo}','${tempmin}','${tempmax}','${dia}')">
                                                             <div class="card">
                                                                 <div id="Dia">
                                                                 ${dia}
                                                                 </div>
                                                             </div>
                                                         </div>`
-                    
-                     
-                }
-            
+
+
             }
 
         }
-       
+
+    }
+
 
 }
 //função Para alterar o modal com informações especificas do tempo
-function alteramodal(tempo,tempomin,tempomax,dia){
+function alteramodal(tempo, tempomin, tempomax, dia) {
     document.querySelector('#ibge').innerHTML = `Minima: ${tempomin}c<br> Maxíma: ${tempomax}c `
     document.querySelector('#populacao').innerHTML = `Tempo: ${tempo}`
     document.querySelector('#obser').innerHTML = `Dia: ${dia}`
 
-    if(tempo == "nublado"){
-        document.querySelector('#modalimg').src= "./img/nublado.jpg"
+    if (tempo == "nublado") {
+        document.querySelector('#modalimg').src = "./img/nublado.jpg"
     }
-    else if(tempo == "céu limpo"){
-        document.querySelector('#modalimg').src= "./img/limpo.jpg"
+    else if (tempo == "céu limpo") {
+        document.querySelector('#modalimg').src = "./img/limpo.jpg"
     }
-    else if( tempo == "chuva leve"){
-        document.querySelector('#modalimg').src= "./img/chuva.jpg"
+    else if (tempo == "chuva leve") {
+        document.querySelector('#modalimg').src = "./img/chuva.jpg"
     }
-    else{
-        document.querySelector('#modalimg').src= "./img/dispersas.jpg"
+    else {
+        document.querySelector('#modalimg').src = "./img/dispersas.jpg"
     }
 
 }
@@ -114,61 +128,60 @@ function recebeDados() {
 
     for (var k in local) {
         item = localStorage.getItem(k)
-        
+
         item = JSON.parse(item)
-        
-        
+
+
         mudado = k.replace(" ", "")
-        link = k.replace(" ","%20")
+        link = k.replace(" ", "%20")
         content = document.querySelector('#content')
         content.innerHTML += `<div class="col col-5"  onclick="iniciamodal('${k}','${item[0]}','${item[1]}','${item[2]}','${item[3]}')">
                                 <div class="card">
                                     <img id="img${mudado}" src="" alt="Chovendo">
                                     <p>${k}</p>
                                     <p class="tempe" id="${mudado}"></p>
+                                    
             
                                 </div>
                               </div>`
-    altera(mudado,link)                       
+        altera(mudado, link, item[0])
     }
 }
 
-recebeDados()
 
-// A função altera, envia os dados da api para o content da primeira pagina
-function altera(dado,lin){
+
+// A função altera, envia os dados da api e imagens para o content da primeira pagina 
+function altera(dado, lin, estado) {
     var req = new XMLHttpRequest();
-        req.onload = mostraApi;
-        req.open("get", `http://api.openweathermap.org/data/2.5/forecast?q=${lin},rs,br&units=metric&lang=pt_br&APPID=c01a54301797a4147060a02f43da82c7`, true);
-        req.send();
-        
-        
-        function mostraApi(){
-            requi = this.responseText
-            requi = JSON.parse(requi)
-                              
-            atual = requi.list[0].main.temp
-            console.log(dado)
-            document.querySelector(`#${dado}`).innerHTML = atual
-            console.log(requi)
-            tempo = requi.list[0].weather[0]['description']
+    req.onload = mostraApi;
+    req.open("get", `http://api.openweathermap.org/data/2.5/forecast?q=${lin},${estado},br&units=metric&lang=pt_br&APPID=c01a54301797a4147060a02f43da82c7`, false);
+    req.send();
 
 
-            //Testando qual imagem deve aparecer.
-            if(tempo == "nublado"){
-                document.querySelector(`#img${dado}`).src= "./img/nublado.jpg"
-            }
-            else if(tempo == "céu limpo"){
-                document.querySelector(`#img${dado}`).src= "./img/limpo.jpg"
-            }
-            else if( tempo == "chuva leve"){
-                document.querySelector(`#img${dado}`).src= "./img/chuva.jpg"
-            }
-            else{
-                document.querySelector(`#img${dado}`).src= "./img/dispersas.jpg"
-            }
-        
-                          }
+    function mostraApi() {
+        requi = this.responseText
+        requi = JSON.parse(requi)
+
+        atual = Math.round(requi.list[0].main.temp)
+        document.querySelector(`#${dado}`).innerHTML = `${atual}°`
+        tempo = requi.list[0].weather[0]['description']
+
+
+        //Testando qual imagem deve aparecer.
+        if (tempo == "nublado") {
+            document.querySelector(`#img${dado}`).src = "./img/nublado.jpg"
+        }
+        else if (tempo == "céu limpo") {
+            document.querySelector(`#img${dado}`).src = "./img/limpo.jpg"
+        }
+        else if (tempo == "chuva leve") {
+            document.querySelector(`#img${dado}`).src = "./img/chuva.jpg"
+        }
+        else {
+            document.querySelector(`#img${dado}`).src = "./img/dispersas.jpg"
+        }
+
+    }
 }
 
 
@@ -177,7 +190,49 @@ function fechamodal() {
     const modal = document.querySelector("#modal-previsao");
     modal.classList.remove('mostrar')
 
+    //apagar informações do modal já cadastradas
     document.querySelector('#dia').innerHTML = ""
+    document.querySelector('#modalimg').src = ""
+}
+
+//Função para editar cidades
+function editarCidade() {
+    local = localStorage
+    local = JSON.stringify(local)
+    local = JSON.parse(local)
+
+
+    for (var k in local) {
+        item = localStorage.getItem(k)
+
+        item = JSON.parse(item)
+
+        content = document.querySelector('#listar')
+        content.innerHTML += `<div class="col col-5"  onclick="selecionaCidade('${k}','${item[0]}','${item[1]}','${item[2]}','${item[3]}')">
+                                    <button class="btn"><p>${k}</p></button>
+                              </div>`
+
+    }
+}
+function selecionaCidade(cidade, estado, ibge, popula, obser) {
+
+    
+    document.querySelector('#selecionada').innerHTML = `<br><br> <p id="cidadeSelecionada">${cidade} selecionada!</p>  <br>`
+    document.querySelector('#cidade').value = cidade
+    document.querySelector('#estado').value = estado;
+    document.querySelector('#ibge').value = ibge;
+    document.querySelector('#populacao').value = popula;
+    document.querySelector('#obser').value = obser;
+
+
+}
+
+
+//função para deletar cidade
+function deletaCidade() {
+    key = document.querySelector('#controle').innerHTML
+    localStorage.removeItem(key);
+    window.location.reload()
 }
 
 //Enviando dados para o localStorage
@@ -185,11 +240,12 @@ function fechamodal() {
 function Adiciona() {
     inputCidade = document.querySelector('#cidade').value;
     estado = document.querySelector('#estado').value;
+    estado = estado.toUpperCase()
     ibge = document.querySelector('#ibge').value;
     populacao = document.querySelector('#populacao').value;
     obser = document.querySelector('#obser').value;
 
-    if (estado == "" || inputCidade == "" || ibge == "" || populacao == "") {
+    if (estado == "" || inputCidade == "") {
         alert("Você deixou campos em branco!")
     }
     else {
@@ -224,36 +280,35 @@ function Adiciona() {
 
 
 }
-//Entrando em contato com a API de previsão de tempo
+//Edita os locais do localStorage
 
-//Foi usada a api https://openweathermap.org/, ela me permite até 60 requisições de cidades por minuto, um valor dificil de ultrapassar para o projeto
-//A api no plano gratuito, me fornce os proximos 5 dias de previsão, com atualização a cada 3 horas.
+function edita() {
+    inputCidade = document.querySelector('#cidade').value;
+    estado = document.querySelector('#estado').value;
+    estado = estado.toUpperCase()
+    ibge = document.querySelector('#ibge').value;
+    populacao = document.querySelector('#populacao').value;
+    obser = document.querySelector('#obser').value;
 
+    if (inputCidade == "") {
+        alert("Você Não selecionou uma cidade! Escolha uma das existentes nos botões no topo.")
+    }
+    else {
 
+        cidade = new Array();
+        cidade.push(estado, ibge, populacao, obser);
 
-/*
-function mostraApi() {
-    requi = this.responseText
-    requi = JSON.parse(requi)
-    console.log(requi)
-    console.log(requi.list[5].weather[0]);
-    console.log(requi.list[5].weather[0]['main']);
-    tempo = requi.list[5].weather[0]['description']
-    tempmin = requi.list[1].main.temp_min
-    tempmax = requi.list[1].main.temp_max
-    total   = requi.list[1].main
-   
-};
+        jsonCidade = JSON.stringify(cidade)
 
-function requisitando() {
+        //verificando se existe alguma key igual 
 
-    var req = new XMLHttpRequest();
-    req.onload = mostraApi;
-    req.open("get", "http://api.openweathermap.org/data/2.5/forecast?q=rio%20grande,rs,br&units=metric&lang=pt_br&APPID=c01a54301797a4147060a02f43da82c7", true);
-    req.send();
+        localStorage.setItem(inputCidade, jsonCidade);
+        alert("Cidade Editada com sucesso!")
 
+        window.location.reload()
+    }
 }
-*/
+
 
 
 
